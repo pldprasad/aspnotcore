fec9b4f19646b9b4de6a029b0d6a7fce5f680dd0
Prasad <laprasad@deloitte.com>
2022-11-23 12:44:33 +0530

    added msi

--- obj/Release/net6.0/Deloitte.MnANextGenAnalytics.WebAPI.AssemblyInfo.cs
+++ obj/Release/net6.0/Deloitte.MnANextGenAnalytics.WebAPI.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("6f4562ef-c244-49d9-973e-f62b06b85359")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Deloitte.MnANextGenAnalytics.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
